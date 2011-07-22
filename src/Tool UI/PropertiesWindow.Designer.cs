9a835cbfab823d6f8149023e75e8ff8b56ad63f6
M1C <M1C@8df8d4a5-ca23-4e0e-b71b-b774847ebf13>
2011-07-22 13:02:21 +0000

    Cosmetic changes in properties-window
    git-svn-id: https://home-i7/svn/dsam@8 8df8d4a5-ca23-4e0e-b71b-b774847ebf13

--- src/Tool UI/PropertiesWindow.Designer.cs
+++ src/Tool UI/PropertiesWindow.Designer.cs
@@ -128,7 +128,7 @@
             this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
             this.tabPage1.Size = new System.Drawing.Size(550, 323);
             this.tabPage1.TabIndex = 0;
-            this.tabPage1.Text = "Page 1";
+            this.tabPage1.Text = "General";
             this.tabPage1.UseVisualStyleBackColor = true;
             // 
             // groupBox2
@@ -416,9 +416,9 @@
             this.tabPage2.Location = new System.Drawing.Point(4, 22);
             this.tabPage2.Name = "tabPage2";
             this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(550, 302);
+            this.tabPage2.Size = new System.Drawing.Size(550, 323);
             this.tabPage2.TabIndex = 1;
-            this.tabPage2.Text = "Page 2";
+            this.tabPage2.Text = "Statistics";
             this.tabPage2.UseVisualStyleBackColor = true;
             // 
             // groupBox1
@@ -608,7 +608,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(582, 396);
+            this.ClientSize = new System.Drawing.Size(574, 392);
             this.Controls.Add(this.tabControl1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
